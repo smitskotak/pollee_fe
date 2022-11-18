59ad4a3c9c6e6b674deb59ad0e1a77a20fb27cf3
Smit Sonani <smit.sonani@kotak.com>
2022-11-18 13:06:23 +0530

    reverted route change

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       routes: appRoutes,
-      initialRoute: RouteNames.createPoll,
+      initialRoute: RouteNames.login,
     );
   }
 }
